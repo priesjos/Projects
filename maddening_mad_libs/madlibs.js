077d90cca3c9ad46538ef8e1fb929d67c04d1ab9
unknown <priesjos000@warren.k12.in.us>
2019-01-15 13:42:47 -0500

    It's starting to work now

--- maddening_mad_libs/madlibs.js
+++ maddening_mad_libs/madlibs.js
@@ -1,10 +1,13 @@
 function generateStoryOne(){
-    var noun1
-    var noun2
-    var verb
-    var adverb
-    var adjective
+    var noun1 = $("#noun1");
+    var noun2 = $("#noun2");
+    var noun3 = $("#noun3");
+    var verb = $("#verb");
+    var adjective = $("#adjective");
 
+    var story = "There was a time when " + noun1 + " and " + noun2 + " were plentiful in this world. However, as " + noun3 + " passed, so did their place in this world, and all has " + verb + " since then. Whatever the future holds is " + adjective + ".";
+    $("#story-title").text("STORY ONE");
+    $("#story").text(story);
 }
 
 
