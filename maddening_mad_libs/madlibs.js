e72db90a2b3b6fd289b57da3e79ee2aa0d051d99
unknown <priesjos000@warren.k12.in.us>
2019-01-17 13:42:36 -0500

    need to touch up on website looks and that's it

--- maddening_mad_libs/madlibs.js
+++ maddening_mad_libs/madlibs.js
@@ -2,8 +2,8 @@ function generateStoryOne(){
     var noun1 = $("#noun1").val();
     var noun2 = $("#noun2").val();
     var noun3 = $("#noun3").val();
-    var verb = $("#verb").val();
-    var adjective = $("#adjective").val();
+    var verb = $("#1-verb").val();
+    var adjective = $("#1-adjective").val();
 
     var story1 = "There was a time when " + noun1 + " and " + noun2 + " were plentiful in this world. However, as " + noun3 + " passed, so did their place in this world, and all has " + verb + " since then. Whatever the future holds is " + adjective + ".";
     $("#story-title").text("STORY ONE");
@@ -13,15 +13,16 @@ function generateStoryOne(){
 function generateStoryTwo(){
     var noun_possessive = $("#noun_possessive").val();
     var noun_plural = $("#noun_plural").val();
-    var time
-    var verb
-    var noun_place
+    var time = $("#time").val();
+    var verb = $("#2-verb").val();
+    var noun_place = $("#noun_place").val();
     var emotion = $("#emotion").val();
     var reaction = $("#reaction").val();
     var number_rank = $("#number_rank").val();
-    var adjective  = $("#adjective2").val();
+    var adjective1 = $("#2-adjective1").val();
+    var adjective2  = $("#2-adjective2").val();
 
-    var story2 = "“I dreamt I cut off a " + noun_possessive + " " + noun_plural + " last night,” he recounted. “One by one. Snip by snip.” He continued, “Was I forced to do this, or was it for " + emotion + ", in my dream? I couldn’t tell.” Mild " + reaction + " spattered the banquet hall. He would go on to take " + number_rank + " place in the All-County Regional " + adjective + " Pickup Line Invitational.";
+    var story2 = "“I dreamt I cut off a " + noun_possessive + " " + noun_plural + " at  " + time + ",” he recounted. “One by one. Snip by snip.” He " + verb + ", “Was I forced to do this, or was it for " + emotion + ", in my dream? I couldn’t tell.” " + adjective1 +" " + reaction + " spattered the " + noun_place + ". He would go on to take " + number_rank + " place in the All-County Regional " + adjective2 + " Pickup Line Invitational.";
     $("#story-title").text("STORY TWO");
     $("#story").text(story2);
 }
