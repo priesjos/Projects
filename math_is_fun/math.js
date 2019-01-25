589b82cf17afadb212e5bf159a461e88b7570460
unknown <priesjos000@warren.k12.in.us>
2019-01-25 13:25:49 -0500

    Basic layout created

---
+++ math_is_fun/math.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    $("#min-convert").click(function(){
+        minToYearsDays();
+    });
+
+    $("#calc-grat-total").click(function(){
+        calcGratTotal();
+    });
+
+    $("#calc-bmi").click(function(){
+        calcBMI();
+    });
+
+    $("#hrs-convert").click(function(){
+        decToHoursMins();
+    })
+});
+
+
+function minToYearsDays(){
+
+    alert("minToYearsDays");
+    
+}
+
+function calcGratTotal(){
+
+    alert("calcGratTotal");
+
+}
+
+function calcBMI(){
+
+    alert("calcBMI");
+
+}
+
+function decToHoursMins(){
+
+    alert("decToHoursMins");
+
+}
