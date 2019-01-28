be1cc1d695d433c70626d35005edf917dccf94fc
unknown <priesjos000@warren.k12.in.us>
2019-01-28 13:44:38 -0500

    JS virtually finished, 4th one needs completion

--- math_is_fun/math.js
+++ math_is_fun/math.js
@@ -19,47 +19,45 @@ $(document).ready(function(){
 
 function minToYearsDays(){
 
-    var minutes;
-    var days;
-    var years;
+    var minutes = $("#minutes-convert").val();
+    var days = Math.floor(minutes/60/24);
+    var years = Math.floor(days/365);
+    var daysLeft = days%365;
 
-    minutes = $("#minutes-convert").val();
-
-    days = Math.floor(minutes/60/24);
-    years = Math.floor(days/365);
-
-    $("#output-years-days").text(years + " years, " + days + " days");
+    $("#output-years-days").text(years + " years, " + daysLeft + " days");
 
 }
 
 function calcGratTotal(){
 
-    var subtotal;
-    var gratuity;
-    var outputGrat;
-    var total;
+    var subtotal = parseInt($("#subtotal").val());
+    var gratuity = parseInt($("#grat").val());
+    var outputGrat = subtotal*(gratuity/100);
+    var total = subtotal + outputGrat;
 
-    var subtotal = $("#subtotal").val();
-    var gratuity = $("#grat").val();
-
-    
-
-
-    $("#output-grat-total").text();
+    $("#output-grat-total").text("Gratuity: $" + outputGrat + ", Total: $" + total);
 
 }
 
 function calcBMI(){
 
-    var inputOne = $("#pounds").val();
-    var inputTwo = $("#inches").val();
+    var pounds = $("#pounds").val();
+    var inches = $("#inches").val();
+    var kilograms = pounds*.45359237;
+    var meters = inches*.0254;
+    var bmi = kilograms/(Math.pow(meters,2));
 
-    $("#output-bmi").text();
+    $("#output-bmi").text(bmi);
+
 }
 
 function decToHoursMins(){
 
-    var input = $("#hrs-float").val();
+    var hours = $("#hrs-float").val();
+    var tenth = (Math.floor(hours*100/10))%10;
+    var hundredth = (Math.floor(hours*100))%10;
+    var decimalDigits = tenth + "" + hundredth;
+    var test = Math.round((decimalDigits*60)/100);
 
-    $("#output-hrs-mins").text();
+    $("#output-hrs-mins").text(test);
 }
