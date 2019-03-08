c859797b205b4ddac0aa7304c0c3200385193bbf
unknown <priesjos000@warren.k12.in.us>
2019-03-08 13:44:08 -0500

    work

--- tic_tac_toe/tic_tac_toe.js
+++ tic_tac_toe/tic_tac_toe.js
@@ -1,5 +1,33 @@
 $(document).ready(function(){
 
+    var turn = 1;
 
+    $("td").click(function(){
+
+        if($(this).text() === ""){
+            if(turn%2 === 1){
+                $(this).text() === "X"
+            }
+            else {
+                $(this).text() === "O"
+            }
+            turn++;
+        }
+
+    })
+
+    $("button").click(function(){
+        turn = 1;
+    })
     
 })
+
+var checkWin = function(){
+
+
+}
+
+var reset = function(){
+
+
+}
