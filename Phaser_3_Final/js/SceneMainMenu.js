a860c6f8267df467796ca193b4067fba859c22a8
unknown <priesjos000@warren.k12.in.us>
2019-05-23 13:20:42 -0400

    rough main menu

--- Phaser_3_Final/js/SceneMainMenu.js
+++ Phaser_3_Final/js/SceneMainMenu.js
@@ -12,7 +12,17 @@ class SceneMainMenu extends Phaser.Scene
         this.block = this.add.sprite(this.game.config.width * 0.5, this.game.config.height * 0.5, "block");
         this.block.setInteractive();
         this.block.on("pointerdown", function(){this.scene.start("Scene1")}, this);
-        this.text = this.add.text(this.game.config.width * 0.435, this.game.config.height * 0.6, 'Click this thing', {fontSize:'14px', fill:'#FFF'});
+        this.add.text(this.game.config.width * 0.40, this.game.config.height * 0.1, 'HACK & SLASH PLATFORMER', {fontSize:'14px', fill:'#FFF'});
+        //giant list of text
+        this.add.text(this.game.config.width * 0.2, this.game.config.height * 0.15, 'CONTROLS', {fontSize:'14px', fill:'#FFF'});
+        this.add.text(this.game.config.width * 0.2, this.game.config.height * 0.2, 'Light Attack: A', {fontSize:'14px', fill:'#FFF'});
+        this.add.text(this.game.config.width * 0.2, this.game.config.height * 0.25, 'Med Attack: S', {fontSize:'14px', fill:'#FFF'});
+        this.add.text(this.game.config.width * 0.2, this.game.config.height * 0.3, 'Hard Attack: D', {fontSize:'14px', fill:'#FFF'});
+        this.add.text(this.game.config.width * 0.2, this.game.config.height * 0.35, 'Dash: Space', {fontSize:'14px', fill:'#FFF'});
+        this.add.text(this.game.config.width * 0.2, this.game.config.height * 0.4, 'Jump: Up Arrow', {fontSize:'14px', fill:'#FFF'});
+        this.add.text(this.game.config.width * 0.2, this.game.config.height * 0.45, 'Crouch: Down Arrow', {fontSize:'14px', fill:'#FFF'});
+        this.add.text(this.game.config.width * 0.2, this.game.config.height * 0.5, 'Move: Left/Right Arrows', {fontSize:'14px', fill:'#FFF'});
+        this.add.text(this.game.config.width * 0.42, this.game.config.height * 0.6, 'Click this to start', {fontSize:'14px', fill:'#FFF'});
     }
 
 }
